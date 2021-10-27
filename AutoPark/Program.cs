190b0f5c1d58da6bdfe728f1079aebaca191636c
ASUS <maksim.maksimeika@gmail.com>
2021-10-27 22:28:26 +0300

    Add VehicleType class.

--- AutoPark/Program.cs
+++ AutoPark/Program.cs
@@ -4,9 +4,77 @@ namespace AutoPark
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //1 task
+            var vehicleTypes = new VehicleType[]
+            {
+                new VehicleType("Bus", 1.2d),
+                new VehicleType("Car", 1d),
+                new VehicleType("Rink", 1.5d),
+                new VehicleType("Tractor", 1.2d)
+            };
+
+            //2 task
+            foreach (var vehicleType in vehicleTypes)
+            {
+                vehicleType.Display();
+            }
+
+            //3task
+            vehicleTypes[^1].TaxCoefficient = 1.3d;
+
+            //4task
+            Console.WriteLine($"\nMax coefficient is {FindMaxTaxCoefficient(vehicleTypes)}");
+
+            //5 task
+            Console.WriteLine($"Average coefficient is {CalculateAverageTaxCoefficient(vehicleTypes)}\n");
+
+            //6 task
+            var sum = 0d;
+            var maxCoefficient = 0d;
+            foreach (var vehicleType in vehicleTypes)
+            {
+                vehicleType.Display();
+                if (vehicleType.TaxCoefficient > maxCoefficient)
+                {
+                    maxCoefficient = vehicleType.TaxCoefficient;
+                }
+                sum += vehicleType.TaxCoefficient;
+            }
+            var average = sum / vehicleTypes.Length;
+
+            Console.WriteLine($"\nMax coefficient is {FindMaxTaxCoefficient(vehicleTypes)}" +
+                    $"Average coefficient is {CalculateAverageTaxCoefficient(vehicleTypes)}\n");
+
+            //7 task
+            foreach (var vehicleType in vehicleTypes)
+            {
+                Console.WriteLine(vehicleType);
+            }
+        }
+
+        public static double FindMaxTaxCoefficient(VehicleType[] vehicleTypes)
+        {
+            var maxCoefficient = 0d;
+            foreach (var item in vehicleTypes)
+            {
+                if (item.TaxCoefficient > maxCoefficient)
+                {
+                    maxCoefficient = item.TaxCoefficient;
+                }
+            }
+            return maxCoefficient;
+        }
+
+        public static double CalculateAverageTaxCoefficient(VehicleType[] vehicleTypes)
+        {
+            var sum = 0d;
+            foreach (var vehicleType in vehicleTypes)
+            {
+                sum += vehicleType.TaxCoefficient;
+            }
+            return sum / vehicleTypes.Length;
         }
     }
 }
---
+++ AutoPark/VehicleType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPark
+{
+    public class VehicleType
+    {
+
+        public VehicleType() { }
+
+        public VehicleType(string typeName,
+                double taxCoefficient)
+        {
+            TypeName = typeName;
+            TaxCoefficient = taxCoefficient;
+        }
+
+        public string TypeName { get; set; }
+        public double TaxCoefficient { get; set; }
+
+        public override string ToString() => $"{TypeName}; {TaxCoefficient}";
+        public void Display()
+        {
+            Console.WriteLine($"typeName = {TypeName} - taxCoefficient = {TaxCoefficient}");
+        }
+    }
+}
