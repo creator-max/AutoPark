cbcda59d3df7c2050764e360568028f5d2ec8835
ASUS <maksim.maksimeika@gmail.com>
2021-11-01 17:56:36 +0300

    Add Engines (Electrical, Combustion).

---
+++ AutoPark/CombustionEngine.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoPark
+{
+    public class CombustionEngine : Engine
+    {
+        private const double CoefficientFuelConsumption = 100d;
+        public CombustionEngine(string typeName, double taxCoefficient)
+            : base(typeName, taxCoefficient)
+        {
+
+        }
+        public double EngineCompacityInCubCM { get; private protected set; }
+        public double FuelConsumptionPer100 { get; private protected set; }
+
+        public double GetMaxKilometers(double fuelTankCapacity)
+            => fuelTankCapacity / FuelConsumptionPer100 * CoefficientFuelConsumption;
+    }
+}
---
+++ AutoPark/GasolineEngine.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoPark
+{
+    public class GasolineEngine : CombustionEngine
+    {
+        public GasolineEngine(double engineCompacityInCubCM, double fuelConsumptionPer100)
+            : base("Gasoline", 1)
+        {
+            EngineCompacityInCubCM = engineCompacityInCubCM;
+            FuelConsumptionPer100 = fuelConsumptionPer100;
+        }
+    }
+}
