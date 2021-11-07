91021ed53d652771e48b4911980e1b5069ac709e
ASUS <maksim.maksimeika@gmail.com>
2021-11-07 19:30:43 +0300

    Update VehicleType class.

--- AutoPark/VehicleType.cs
+++ AutoPark/VehicleType.cs
@@ -4,16 +4,18 @@ namespace AutoPark
 {
     public class VehicleType
     {
-
         public VehicleType() { }
 
-        public VehicleType(string typeName,
+        public VehicleType(int id,
+                string typeName,
                 double taxCoefficient)
         {
+            Id = id;
             TypeName = typeName;
             TaxCoefficient = taxCoefficient;
         }
 
+        public int Id { get; }
         public string TypeName { get; set; }
         public double TaxCoefficient { get; set; }
 
