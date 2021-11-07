a048b3f0156d02067861fbdf2ffc0f6940913f96
ASUS <maksim.maksimeika@gmail.com>
2021-11-07 19:03:22 +0300

    Add Rent class.

---
+++ AutoPark/Rent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AutoPark
+{
+    public class Rent
+    {
+        public Rent()
+        {
+
+        }
+
+        public Rent(DateTime dateOfRent, double price)
+        {
+            DateOfRent = dateOfRent;
+            Price = price;
+        }
+
+        public DateTime DateOfRent { get; }
+        public double Price { get; }
+    }
+}
