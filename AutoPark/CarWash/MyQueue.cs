3a4d5cba6b138eada7dffa965ef98d5cdd7e5fbb
ASUS <maksim.maksimeika@gmail.com>
2021-11-13 00:54:57 +0300

    Add queue (car wash).

---
+++ AutoPark/CarWash/MyQueue.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AutoPark.CarWash
+{
+    public class MyQueue<T> : IEnumerable<T>
+    {
+        private const int DefaultSize = 10;
+        private int _firstIndex;
+        private int _lastIndex;
+        private T[] _queue;
+
+        public MyQueue()
+        {
+            _queue = new T[DefaultSize];
+        }
+
+        public MyQueue(int size)
+        {
+            if(size <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _queue = new T[size];
+        }
+
+        public MyQueue(IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _queue = new T[DefaultSize];
+            foreach(var item in collection)
+            {
+                Enqueue(item);
+            }
+
+        }
+
+        public int Count { get; set; }
+
+        public void Enqueue(T item)
+        {
+            if(Count == _queue.Length)
+            {
+                SetCapacity(_queue.Length * 2);
+            }
+            if(Count == 0)
+            {
+                _queue[_lastIndex] = item;
+            }
+            else
+            {
+                _queue[++_lastIndex] = item;
+            }
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if(Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            var dequeueItem = _queue[_firstIndex];
+            _firstIndex++;
+            Count--;
+            return dequeueItem;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_queue, 0, _queue.Length);
+        }
+
+        public bool Contains(T item)
+        {
+            for(int i = _firstIndex; i <= _lastIndex; i++)
+            {
+                if (_queue[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var i = _firstIndex;
+
+            while(i <= _lastIndex)
+            {
+                yield return _queue[i++];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private void SetCapacity(int capacity)
+        {
+            if (_queue == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var newQueue = new T[capacity];
+
+            Array.Copy(_queue, newQueue, _queue.Length);
+            _queue = newQueue;
+        }
+    }
+}
--- AutoPark/Program.cs
+++ AutoPark/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using AutoPark.CarWash;
 
 namespace AutoPark
 {
@@ -9,29 +10,25 @@ namespace AutoPark
         {
             string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\Data\"));
             var collections = new Collections(
-                    $"{path}types.csv", 
-                    $"{path}vehicles.csv", 
+                    $"{path}types.csv",
+                    $"{path}vehicles.csv",
                     $"{path}rents.csv");
 
-            //1 task
-            collections.Print();
+            var vehicles = collections.Vehicles;
+            var washQueue = new MyQueue<Vehicle>(vehicles.Count);
 
-            //2 task
-            var vehicle = new Vehicle(1, collections.VehicleTypes[0], new GasolineEngine(2d, 8.1d), "Volkswagen Crafter", "5427 AX-7", 2022d, 2015, 376000d, Color.Blue, 500d);
-            collections.Insert(collections.Vehicles.Count - 1, vehicle);
+            foreach(var vehicle in vehicles)
+            {
+                washQueue.Enqueue(vehicle);
+                Console.WriteLine($"{vehicle.Model} ({vehicle.LicensePlat}) arrived at the wash.");
+            }
 
-            //3 task
-            collections.Delete(1);
-            collections.Delete(4);
-
-            //4 task
-            collections.Print();
-
-            //5 task
-            collections.Sort(new VehicleComparer());
-
-            //6 task
-            collections.Print();
+            Console.WriteLine();
+            while(washQueue.Count > 0)
+            {
+                var vehicle = washQueue.Dequeue();
+                Console.WriteLine($"{vehicle.Model} ({vehicle.LicensePlat}) was washed.");
+            }
         }
     }
 }
